fe6bc1c16a3845ce6dd90b6415e0c9ee356cd9f8
Marvin <bugeyes.de@gmail.com>
2020-05-25 17:42:12 +0200

    progress

--- ressourcen/modal.js
+++ ressourcen/modal.js
@@ -1,39 +1,31 @@
 const modal = document.getElementById("modal");
 const closeBtn = document.getElementById('closeBtn');
 
-export const openModal = (t) => {
-    console.log(t);
-    insideModal(t)
-    modal.style.display = "block";
+export const openModal = (data) => {
+  insideModal(data);
+  modal.style.display = "block";
 }
 
 const closeModal = () => {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 closeBtn.addEventListener("click", closeModal);
 
 //generate individual content for each project modal
-export let insideModal = (projectContent) => {
-    if(document.getElementById('modalContent')){
-        document.getElementById('modalContent').remove();}
-    console.log(projectContent);
-    let modalContent = document.createElement('div');
-    modalContent.id = 'modalContent';
-    let project = projectContent.value;
-        let pic = [];
-        for (let i=0; i < project.images.length; i++){
-            pic[i] = document.createElement('img');
-            pic[i].src = project.images[i].src;
-            modalContent.appendChild(pic[i]);
-        }
+export const insideModal = data => {
+  const content = document.getElementById('modalContent')
+  const images = content.querySelector('.modalContentImages')
 
-    let title = document.createElement('h2');
-    title.innerHTML = project.title;
-    let description = document.createElement('p');
-    description.innerHTML = project.description;
-    modalContent.appendChild(title);
-    modalContent.appendChild(description);
+  images.innerHTML = "";
 
-    modal.appendChild(modalContent);
+  content.querySelector('h3').innerHTML = data.title;
+  content.querySelector('p').innerHTML = data.description;
+
+  for (let i=0; i < data.images.length; i++) {
+    let pic;
+    pic = document.createElement('img');
+    pic.src = data.images[i].src;
+    images.appendChild(pic);
+  }
 }
--- ressourcen/projects.js
+++ ressourcen/projects.js
@@ -9,7 +9,6 @@ const loadProjects = async(url) => {
 const renderProjects = content => {
 
   //get number of projects / create project array
-  console.log(content);
   let numberOfProjects = content.projects.length;
   let projects = [];
   for(let i=1; i < numberOfProjects+1; i++){
@@ -18,9 +17,7 @@ const renderProjects = content => {
 
   //pick random project and put it up front
   let randomProject = Math.floor(Math.random() *numberOfProjects);
-  console.log(randomProject);
   let chosenOne = projects.slice(randomProject, 1);
-  console.log(chosenOne);
   // add titles and pics to main
 
   let newProject = (num) => {
@@ -31,20 +28,18 @@ const renderProjects = content => {
     let img = document.createElement('img');
     img.src = 'ressourcen/img/spacecat' + num + '.jpg';
 
-
     //Sets the first chosen Project as main preview
     if(firstPic) {
       project.className = 'project mainPro'
       firstPic = false;
     }
 
-      project.id = num;
-      //Project specific content from JSON file as *.value to pass it to modal.js
-      project.value = content.projects[num-1];
-
+    project.id = num;
+    //Project specific content from JSON file as *.value to pass it to modal.js
+    project.value = content.projects[num-1];
 
     project.appendChild(img);
-    project.addEventListener("click", function(e){openModal(e.currentTarget)});
+    project.addEventListener("click", function() { openModal(content.projects[num-1]) });
 
     let title = document.createElement('h2');
     title.innerHTML = content.projects[num -1].title;
